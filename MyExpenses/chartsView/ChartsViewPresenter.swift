65500069409cb4223826819be5881df6af325f85
Toan <chan.misha95@mail.ru>
2023-04-26 03:41:18 +0300

    - Изменил логику показа графика

--- MyExpenses/chartsView/ChartsViewPresenter.swift
+++ MyExpenses/chartsView/ChartsViewPresenter.swift
@@ -9,23 +9,39 @@ import UIKit
 import Charts
 class ChartsViewPresenter {
     var expenses = CoreDataManager.shared.fetchNotes()
-    
-    func configureEntries()-> [PieChartDataEntry] {
-        let enties = expenses.compactMap { $0.coast }
-        let entriesValue = enties.compactMap { Double($0) }
-        let entries = entriesValue.compactMap { PieChartDataEntry(value: Double($0)) }
-        return entries
-    }
-    
     func setData() -> PieChartData {
         let set = PieChartDataSet(entries: configureEntries())
-        set.colors = [UIColor.red, UIColor.gray, UIColor.blue]
+        set.colors = [UIColor.red, UIColor.green, UIColor.magenta]
         let data = PieChartData(dataSet: set)
         return data
     }
     
-    func test() {
-        let entries = expenses.compactMap { $0.type }
-        print(entries)
+    func configureEntries() -> [PieChartDataEntry] {
+        let redType = expenses.filter { $0.typeColor == .red }
+        let redCoastString = redType.compactMap { $0.coast }
+        let redCoast = redCoastString.compactMap { Double($0) }
+        let sumRedCoast = redCoast.reduce(0) { partialResult, value in
+            partialResult + value
+        }
+        
+        let greenType = expenses.filter { $0.typeColor == .green }
+        let greenCoastString = greenType.compactMap { $0.coast }
+        let greenCoast = greenCoastString.compactMap { Double($0) }
+        let sumGreenCoast = greenCoast.reduce(0) { partialResult, value in
+            partialResult + value
+        }
+        
+        let magentaType = expenses.filter { $0.typeColor == .magenta }
+        let magentaCoastString = magentaType.compactMap { $0.coast }
+        let magentaCoast = magentaCoastString.compactMap { Double($0) }
+        let sumMagentaCoast = magentaCoast.reduce(0) { partialResult, value in
+            partialResult + value
+        }
+        
+        var entries: [PieChartDataEntry] = Array()
+        entries.append(PieChartDataEntry(value: sumRedCoast, label: "Red Value"))
+        entries.append(PieChartDataEntry(value: sumGreenCoast, label: "Green Value"))
+        entries.append(PieChartDataEntry(value: sumMagentaCoast, label: "Magenta Value"))
+        return entries
     }
 }
